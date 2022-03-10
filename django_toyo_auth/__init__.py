4c00f192c60e1e651bae64253eccf24485f851d4
Ayame Tatematsu <ayame.space@gmail.com>
2022-03-10 19:51:04 +0900

    django 3.2未満でもsignalsを読み込めるように

--- django_toyo_auth/__init__.py
+++ django_toyo_auth/__init__.py
@@ -1,6 +1,11 @@
 __copyright__    = 'Copyright (C) 2021 ayame.space'
-__version__      = '0.3.0'
+__version__      = '0.3.1'
 __license__      = 'MIT'
 __author__       = 'ayame.space'
 __author_email__ = 'ayame.space@gmail.com'
 __url__          = 'http://github.com/ayame-q/django-toyo-auth'
+
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = "django_toyo_auth.apps.AccountsConfig"
